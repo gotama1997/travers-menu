96e894253e2414bb99ceba16c1530c2e303a00bc
gotama1997 <64990791+gotama1997@users.noreply.github.com>
2020-07-30 00:34:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-for (const item1 of document.getElementsByClassName('nav-list nav-list-root')[0].children) {
-    if(item1){
-        item1.children[0].id = "N1" ;
-   var text1 =  item1.children[0].children[0].innerText;
-    if(item1.children[1]){
-        for (const item2 of item1.children[1].children[0].children) {
-            if(item2.className!="separator"&&item2.children[0]){
-                item2.children[0].children[0].id = "N2 - " +text1 ;
-                let text2 = item2.children[0].children[0].innerText;
-                for (const item3 of item2.children[0].children[1].children) {
-                    item3.children[0].id = "N3 - "+text2+" - "+text1;
+    for (const itr1 of document.getElementsByClassName("nav-item-root")) {
+        var text1 = itr1.children[0].children[0].innerText;
+        itr1.children[0].id = "N1 - "+text1;
+        for (const itr2 of itr1.children[1].children[0].children) {
+            if(itr2.className!="separator"){
+                var text2 = itr2.children[0].children[0].innerText;
+                itr2.children[0].children[0].id="N2 - "+text2+" - "+text1;
+                if(text1!="Catálogos en Línea"){
+for (const itr3 of itr2.children[0].children[1].children) {
+                   var text3 = itr3.children[0].innerText;
+                   itr3.children[0].id = "N3 - "+text3+" - "+text2+" - "+text1;
+                   console.log(text3);
                 }
+                }
+                
             }
         }
     }
-    }
-}
